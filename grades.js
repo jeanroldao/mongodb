ac8c8388a3c22b2e9a7d4849f4fc7a7a55ac5c2a
Jean Farias Roldão <jeanroldao@gmail.com>
2015-11-16 22:18:36 -0200

    grades query
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

---
+++ grades.js
@@ -0,0 +1,18 @@
+db.grades.aggregate([
+  {$unwind:'$scores'},
+  {$match: {'scores.type' : {$ne: 'quiz'}}},
+  {
+    $group:{
+      _id: {student_id:'$student_id', class_id:'$class_id'},
+      student_avarage: {$avg:'$scores.score'}
+    }
+  },
+  {
+    $group:{
+      _id: '$_id.class_id',
+      class_avarage: {$avg: '$student_avarage'}
+    }
+  },
+  {$sort: {class_avarage:-1}},
+  {$limit:1}
+]);
